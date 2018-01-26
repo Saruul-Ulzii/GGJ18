44d2aff4a7f15f647a011e5e2ee5856d40f44be2
theSoenke <theSoenke@users.noreply.github.com>
2018-01-26 20:16:50 +0100

    Init Websocket server

---
+++ Game/Assets/Scripts/Server.cs
@@ -0,0 +1,21 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class Server : WebSocketBehavior
+{
+	public void Start() {
+        var wssv = new WebSocketServer("ws://dragonsnest.far");
+        wssv.AddWebSocketService<Server>("/Laputa");
+        wssv.Start();
+        wssv.Stop();
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        var msg = e.Data == "BALUS"
+                  ? "I've been balused already..."
+                  : "I'm not available now.";
+
+        Send(msg);
+    }
+}
