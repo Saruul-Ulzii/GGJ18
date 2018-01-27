f179207f7a6ef61930cc71e4e03e50492a67bffb
Unknown <nilsw.agner@web.de>
2018-01-27 11:45:40 +0100

    added crude qr reader + refac

--- Controller/Assets/Scripts/VerticalWebSocket.cs
+++ Controller/Assets/Scripts/VerticalWebSocket.cs
@@ -5,33 +5,55 @@ using System;
 
 public class VerticalWebSocket : WSClientBehaviour
 {
+    public WebsocketState State {get;private set;}    
+
+    public Nullable<int> PlayerId {get; private set;}
+    public string PlayerName {get; private set;}
 
 	// Use this for initialization
 
     void Start () {
         string url = "ws://172.18.11.187:5001/Server";
+        State = WebsocketState.UnInitialized;
         connect(url );
-
-
     }
 
     public override void handleCommand(Command c)
     {
         base.handleCommand(c);
-        Debug.Log(c.Name);
-        Debug.Log(c.Content);
+        
+        switch(c.Name.ToLower())
+		{
+			case "id":
+				HandleIdCommand(c.Content.ToLower());
+                break;
+		}
+
     }
+
+    private void HandleIdCommand(string arg)
+    {
+        if(State != WebsocketState.NameSent)
+        {
+            Debug.Log("Wrong context for command");
+            return;
+        }
+        State = WebsocketState.Initialized;
+        PlayerId = int.Parse(arg);
+    }
+
+
 
     public override void onConnectionReady(object sender, EventArgs e)
     {
         base.onConnectionReady(sender, e);
-        initializeConnection();
 
     }
 
-    public void initializeConnection()
+    public void SendName(string playerName)
     {
-        Command testCommand = new Command("NAME", "Fuck the mainstream");
-        sendCommand(testCommand);
+        Command cmd = new Command("NAME", playerName);
+        PlayerName = playerName;
+        sendCommand(cmd);
     }
 }
