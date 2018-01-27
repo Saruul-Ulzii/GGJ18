d28ad6fd112e220733c9cd043b44ff9905857c5e
Unknown <tobias.hendricks@gmx.de>
2018-01-27 12:06:10 +0100

    Auto stash before merge of "master" and "origin/master"
    Added Gamemanager & LobbyManager

---
+++ Game/Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    public GameObject PlayerEntryPrefab;
+    public Transform PlayerEntryListTransform;
+
+    public List<LobbyPlayerEntry> LobbyPlayer;
+
+    private void Start()
+    {
+        LobbyPlayer = new List<LobbyPlayerEntry>();
+    }
+
+    public void AddPlayer(Player player)
+    {
+        var instance = Instantiate(PlayerEntryPrefab, PlayerEntryListTransform);
+        var entry = instance.GetComponent<LobbyPlayerEntry>();
+        entry.AssignPlayer(player);
+        LobbyPlayer.Add(entry);
+    }
+}
---
+++ Game/Assets/Scripts/Lobby/LobbyPlayerEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyPlayerEntry : MonoBehaviour
+{
+    private Player _player;
+    public Text PlayerNameText;
+    public Image PlayerColorImage;
+    
+    public void AssignPlayer(Player player)
+    {
+        _player = player;
+        PlayerNameText.text = player.Name;
+        PlayerColorImage.color = player.Color;
+    }
+
+}
