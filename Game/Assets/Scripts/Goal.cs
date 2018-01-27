818b0cf188202ad25bcfdba1cadbd9310641fc13
MININT-7C70DMO\Sergej <sergejkern@posteo.de>
2018-01-27 18:51:50 +0100

    added winScreen yo

---
+++ Game/Assets/Scripts/Goal.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<SpaceShipController>() != null)
+        {
+            SceneManager.LoadScene("WinScreen");
+        }
+    }
+}
---
+++ Game/Assets/Scripts/WinScreenUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenUI : MonoBehaviour {
+
+    public void ReVerticalizeClicked () {
+        SceneManager.LoadScene("websocket");
+	}
+}
