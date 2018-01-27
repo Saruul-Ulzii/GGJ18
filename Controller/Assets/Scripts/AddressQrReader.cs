55d0ca0f05bc934a024ad5a288f0839390e453c4
Cyberlander <felix.froehlich1@gmx.net>
2018-01-27 12:15:47 +0100

    frame counter

--- Controller/Assets/Scripts/AddressQrReader.cs
+++ Controller/Assets/Scripts/AddressQrReader.cs
@@ -15,12 +15,15 @@ public class AddressQrReader : MonoBehaviour {
 	public VerticalWebSocket Ws;
 	public GameObject Next;
 
+    public Int32 counter;
+
 	void Start() 
 	{
 		screenRect = new Rect(0, 0, Screen.width, Screen.height);
 		camTexture = new WebCamTexture();
 		camTexture.requestedHeight = Screen.height; 
 		camTexture.requestedWidth = Screen.width;
+        counter = 0;
 		if (camTexture != null) 
 		{
 			camTexture.Play();
@@ -30,23 +33,29 @@ public class AddressQrReader : MonoBehaviour {
 	void OnGUI () {
 		// drawing the camera on screen
 		GUI.DrawTexture (screenRect, camTexture, ScaleMode.ScaleToFit);
-		// do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
-		try 
-		{
-			IBarcodeReader barcodeReader = new BarcodeReader ();
-			// decode the current frame
-			var result = barcodeReader.Decode(camTexture.GetPixels32(),
-			camTexture.width, camTexture.height);
-			if (result != null) 
-			{
-				Next.SetActive(true);
-				gameObject.SetActive(false);
-				Ws.connect(result.Text);
-			}
+        counter += 1;
+        // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+        if (counter % 10 == 0)
+        {
+            try
+            {
+                IBarcodeReader barcodeReader = new BarcodeReader();
+                // decode the current frame
+                var result = barcodeReader.Decode(camTexture.GetPixels32(),
+                camTexture.width, camTexture.height);
+                if (result != null)
+                {
+                    Next.SetActive(true);
+                    gameObject.SetActive(false);
+                    Ws.connect(result.Text);
+                }
 
-		} catch(Exception ex) 
-		{ 
-			Debug.LogWarning (ex.Message); 
-		}
+            }
+            catch (Exception ex)
+            {
+                Debug.LogWarning(ex.Message);
+            }
+        }
+        
 	}
 }
