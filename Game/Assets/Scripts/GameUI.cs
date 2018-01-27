28e5853539c576aa6cde9de797bc1d9c47c1f648
MININT-7C70DMO\Sergej <sergejkern@posteo.de>
2018-01-27 18:29:58 +0100

    Added Healthbar with UI and all that

---
+++ Game/Assets/Scripts/GameUI.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public PlayerShipLife ShipLife;
+    public Image Lifebar;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Lifebar.fillAmount = ShipLife.LifePercent;
+    }
+}
