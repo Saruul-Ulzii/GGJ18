097410449a498200acf9149af0e6a4d407597fda
Unknown <nilsw.agner@web.de>
2018-01-28 15:57:14 +0100

    audio

---
+++ Game/Assets/Scripts/Ship/ShipAudioController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAudioController : MonoBehaviour {
+
+	public AudioSource _thrusterSoundSource;
+	private bool _playing;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var thrusters = (TriebwerkController[]) GameObject.FindObjectsOfType(typeof(TriebwerkController));
+		foreach(var t in thrusters)
+		{
+			if(t.On)
+			{
+				if(!_thrusterSoundSource.isPlaying)
+					_thrusterSoundSource.Play();
+				_playing = true;
+				return;	
+			}
+		}
+		if(_playing)
+		{
+			_thrusterSoundSource.Stop();
+			_playing = false;
+		}
+		
+	}
+}
