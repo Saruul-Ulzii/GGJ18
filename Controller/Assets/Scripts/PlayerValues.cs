423e217dbb27f9f93cc51b0c86d4bb031df79090
theSoenke <theSoenke@users.noreply.github.com>
2018-01-28 13:26:17 +0100

    Use same colors in Client and Server

--- Controller/Assets/Scripts/PlayerValues.cs
+++ Controller/Assets/Scripts/PlayerValues.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerValues : MonoBehaviour
+public class PlayerValues
 {
-    public static PlayerValues Instance;
-
-    public List<Color> colors;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
+    public static readonly List<Color> Colors = new List<Color>(new Color[] {
+        new Color(223/255.0f, 38/255.0f, 38/255.0f),
+        new Color(64/255.0f, 74/255.0f, 143/255.0f),
+        new Color(73/255.0f, 227/255.0f, 106/255.0f),
+        new Color(90/255.0f, 32/255.0f, 32/255.0f),
+        new Color(163/255.0f, 208/255.0f, 3/255.0f),
+        new Color(190/255.0f, 26/255.0f, 206/255.0f)
+    });
 
     public static Color Color
     {
@@ -18,11 +18,11 @@ public class PlayerValues : MonoBehaviour
         {
             if (VerticalWebSocket.PlayerId == null)
             {
-                return Instance.colors[0];
+                return Colors[0];
             }
 
-            var colorID =  VerticalWebSocket.PlayerId.Value % Instance.colors.Count;
-            return Instance.colors[colorID];
+            var colorID = VerticalWebSocket.PlayerId.Value % Colors.Count;
+            return Colors[colorID];
         }
     }
 }
